c1707ade50708306197fe98dd4945151eacd3a08
Kaleb Barton <kaleb.d.barton@ehi.com>
2024-05-14 16:30:44 -0500

    a

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ resource "constellix_a_record" "test_a" {
 
 resource "constellix_a_record_pool" "test_a_pool" {
   count                  = length(local.pools)
+
   name                   = local.pools[count.index].name
   num_return             = "10"
   min_available_failover = 1
@@ -33,10 +34,10 @@ resource "constellix_a_record_pool" "test_a_pool" {
   dynamic "values" {
     for_each = local.pools[count.index].values
     content {
-      value        = values["value"]
-      weight       = values["weight"]
-      policy       = values["policy"]
-      disable_flag = values["disable_flag"]
+      value        = values.value
+      weight       = values.weight
+      policy       = values.policy
+      disable_flag = values.disable_flag
     }
   }
 
