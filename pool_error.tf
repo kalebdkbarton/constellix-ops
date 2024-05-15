08dea1feae44e54d76ccb771fdb2c1e1c713dd09
Kaleb Barton <kaleb.d.barton@ehi.com>
2024-05-14 23:50:08 -0500

    can this be an int?

--- pool_error.tf
+++ pool_error.tf
@@ -1,6 +1,6 @@
 resource "constellix_a_record_pool" "firstrecord" {
   name                   = "firstrecord"
-  num_return             = "1"
+  num_return             = 1
   min_available_failover = 1
   values {
     value        = "8.1.1.1"
