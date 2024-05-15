d69bfc2f775ab25d189af78b3f2ca451a14a055d
Kaleb Barton <kaleb.d.barton@ehi.com>
2024-05-14 21:44:39 -0500

    maybe it's this simple

--- pools.tf
+++ pools.tf
@@ -12,7 +12,7 @@ resource "constellix_a_record_pool" "test_pool" {
       weight       = values.value.weight
       policy       = values.value.policy
       disable_flag = values.value.disable_flag
-      check_id     = constellix_http_check.test_http_check["${count.index}-${values.key}"].id
+      check_id     = constellix_http_check.test_http_check[count.index].id
     }
   }
 
